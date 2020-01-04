e296cdf58d38d8b72f751cd52ecfe0de9b9533c6
Armchair Stravinsky <spid3rbox@gmail.com>
2020-01-04 13:20:31 -0600

    Day 2 Studio Work
    Motors now are stored in an array, and can be addressed by index
    Fixed issue with Duty cycle value overflowing several times

--- motors.ino
+++ motors.ino
@@ -1,19 +1,22 @@
 // Basic sketch for trying out the Adafruit DRV8871 Breakout
 
-#define MOTOR_IN1 5
-#define MOTOR_IN2 6
+#define NUMBEROFWHEELS 2
 
 class Wheel {
   private:
     byte posPin;
     byte negPin;
+    //Percent form of current speed 
     float dutyCycle;
+    //Is motor running?
     bool state;
+    //Is motor getting power in the enabled part of the duty cycle?
     bool engaged;
-    byte cycleTime;
+    int cycleTime;
+    //Last time in milliseconds that the program has checked in on the motor
     unsigned long previousMillis;
-  public:
-    Wheel(byte pos, byte neg, byte cycle) {
+  public: 
+    Wheel(byte pos, byte neg, byte cycle) { 
       posPin = pos;
       negPin = neg;
       dutyCycle = 0.5;
@@ -49,19 +52,24 @@ class Wheel {
       dutyCycle = motorSpeed;
     }
 
-    bool tick(unsigned long currentMillis){
+  bool getState() {
+      return state;
+  }
+
+    void tick(){
       if (not state) {
-        return false;
+        return;
       }
       
       else {
+        unsigned long currentMillis = millis();
         if (engaged) {
           float interval = cycleTime - (dutyCycle * cycleTime);
           if (currentMillis - previousMillis >= interval) {
             digitalWrite(negPin, HIGH);
             engaged = false;
             previousMillis = millis();
-            return true;
+            return;
           }
         }
         else{
@@ -70,21 +78,43 @@ class Wheel {
             digitalWrite(negPin, LOW);
             engaged = true;
             previousMillis = millis();
-            return true;
+            return;
           }
         }
       }
     }
 };
 
-Wheel wheel1 = Wheel(5, 6, 1500);
+
+
+////////////////DECLARE WHEELS HERE////////////////
+
+Wheel wheels[NUMBEROFWHEELS] = 
+  {Wheel(7, 8, 500),
+  Wheel(5, 6, 500)}; 
+
+/////////////END WHEEL DECLARATION//////////////////
+
+
+
+void wheelsOnOff(Wheel *wheels, bool state){
+  for(int i=0; i<NUMBEROFWHEELS; i++){
+    if (state) {
+      wheels[i].on();
+    }
+    else {
+      wheels[i].off();
+    }
+  }
+}
 
 void tickDelay(unsigned long delayTime) {
   unsigned long delayStart = millis();
-  bool ticking = true;
-  while(ticking){
+  while(1){
     unsigned long currentMillis = millis();
-    wheel1.tick(currentMillis);
+    for(int i=0; i < NUMBEROFWHEELS; i++){
+      wheels[i].tick();
+    }
     if (currentMillis - delayStart >= delayTime){
       break;
     }
@@ -93,18 +123,15 @@ void tickDelay(unsigned long delayTime) {
 
 void setup() {
   Serial.begin(9600);
-
   Serial.println("Motor Test Initiated");
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  wheel1.motorSpeed(0.6);
-  wheel1.on();
+  wheels[0].motorSpeed(0.6);
+  wheelsOnOff(wheels, true);
   Serial.println("Motor on");
-  tickDelay(30000);
-  wheel1.off();
+  tickDelay(30000); 
   Serial.println("Motor off");
-  tickDelay(20000);
-  
+  wheelsOnOff(wheels, false);
+  tickDelay(32000);
 }
